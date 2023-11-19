b2168cf5978b1e1f88b4b936b7f629ff89a5396e
Angelo <angelo99franciamore@gmail.com>
2023-11-19 12:29:05 +0100

    chore

--- choropleth_map.py
+++ choropleth_map.py
@@ -6,7 +6,6 @@ with open('data/limits_IT_regions.geojson', encoding='utf-8') as response:
     polygons = json.load(response)
 
 df = pd.read_csv('data/strikes_number_by_region.csv', index_col=0, dtype={"NumeroScioperi": int})
-# df['classificazione'] = df['classificazione'].astype(str)
 
 fig = px.choropleth_mapbox(
     df,
@@ -16,17 +15,10 @@ fig = px.choropleth_mapbox(
     center=dict(lat=41.871941, lon=12.567380),
     color="NumeroScioperi",
     color_continuous_scale="Jet",
-    # color_discrete_sequence=4,
-    # color_discrete_map={'4': 'Green',
-    #                     '3': 'Yellow',
-    #                     '2': 'Orange',
-    #                     '1': 'red'},
-    mapbox_style="carto-positron",
-    zoom=6,
-    range_color=(0, 600),
-    labels={"Classificazione sismica sicilia"},
+    mapbox_style="white-bg",
+    zoom=4.9,
+    range_color=(0, 600)
 )
-
 
 fig.update_geos(fitbounds="locations", visible=False)
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
